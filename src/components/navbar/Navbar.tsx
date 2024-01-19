b5c2556aff4313c0eac923bf92754688b49faf25
Kiev <kevinkiss2010@gmail.com>
2024-01-19 15:46:01 -0300

    Adicionando funcionalidade no icone

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -10,11 +10,12 @@ function Navbar() {
         <div className="container flex justify-between text-lg">
 
           <div className='flex justify-center items-center '>
-            <div className='w-20'>
+          <Link to='/home' className='hover:underline'><div className='w-20'>
               <img src="https://cdn.discordapp.com/attachments/1180161484345585706/1197946685293334581/cabe-acao_-_transparente.png?ex=65bd1dca&is=65aaa8ca&hm=758e45a9d3d63ed018de54b9a4a08ad7dd4ac8d3203c33e867b9dd8a2314ec21&" alt="logo projeto cabe ação" />
-            </div>
-
-            <div className='text-2xl font-bold uppercase'>Cabe Ação</div>
+            </div></Link>
+            <Link to='/home' className=' text-2xl font-bold uppercase'>Cabe Ação</Link>
+            
+          
           </div>
 
 
