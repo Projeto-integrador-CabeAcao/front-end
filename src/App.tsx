5a74b2790cddeb9dd92d889ebb23bb2c1c34a1e3
Kiev <kevinkiss2010@gmail.com>
2024-02-01 11:06:58 -0300

    Adicionando funcionalidades de testes - radio

--- src/App.tsx
+++ src/App.tsx
@@ -7,8 +7,9 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Login from './pages/login/Login';
 import Cadastro from './pages/cadastro/Cadastro';
 import Home from './pages/home/Home';
-import Sobre from './pages/sobre/Sobre';
 import { AuthProvider } from './context/AuthContext';
+import ListaTemas from './components/temas/listaTemas/ListaTemas';
+import FormularioTema from './components/temas/formularioTema/FormularioTema';
 
 
 function App() {
@@ -17,19 +18,21 @@ function App() {
     <AuthProvider>
         <BrowserRouter>
           <Navbar />
-          <div className='min-h-[80vh] bg-[#0488AB]'>
+          <div className='min-h-[80vh]'>
             <Routes>
               <Route path="/" element={<Login />} />
               <Route path="/login" element={<Login />} />
-              <Route path="/sobre" element={<Sobre />} />
               <Route path="/cadastro" element={<Cadastro />} />
               <Route path="/home" element={<Home />} />
+              <Route path="/temas" element={<ListaTemas />} />
+              <Route path="/cadastroTema" element={<FormularioTema />} />
+              <Route path="/editarTema/:id" element={<FormularioTema />} />
+
             </Routes>
           </div>
           <Footer />
         </BrowserRouter>
         </AuthProvider>
-  
     </>
   );
 }
--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -9,31 +9,31 @@ function Navbar() {
   const { usuario, handleLogout } = useContext(AuthContext)
 
   function logout() {
-      handleLogout()
-      alert('Usuário deslogado com sucesso')
-      navigate('/login')
+    handleLogout()
+    alert('Usuário deslogado com sucesso')
+    navigate('/login')
   }
 
   return (
     <>
-      <div className='w-full bg-[#0488AB] text-white flex justify-center py-4'>
+      <div className=' w-full bg-[#0488AB] text-white flex justify-center py-4'>
         <div className="container flex justify-between text-lg">
 
           <div className='flex justify-center items-center '>
-          <Link to='/home' className='hover:underline'><div className='w-20'>
+            <Link to='/home' className='hover:underline'><div className='w-20'>
               <img src="https://cdn.discordapp.com/attachments/1180161484345585706/1197946685293334581/cabe-acao_-_transparente.png?ex=65bd1dca&is=65aaa8ca&hm=758e45a9d3d63ed018de54b9a4a08ad7dd4ac8d3203c33e867b9dd8a2314ec21&" alt="logo projeto cabe ação" />
             </div></Link>
             <Link to='/home' className=' text-2xl font-bold uppercase'>Cabe Ação</Link>
-                     
+
           </div>
 
 
           <div className='flex gap-4'>
-                      
+
             <Link to='/sobre' className='hover:underline'>Sobre</Link>
             <div className='hover:underline'>Postagens</div>
-            <div className='hover:underline'>Temas</div>
-            <div className='hover:underline'>Cadastrar tema</div>
+            <Link to='/temas' className='hover:underline'>Temas</Link>
+            <Link to='/cadastroTema' className='hover:underline'>Cadastrar tema</Link>
             <div className='hover:underline'>Perfil</div>
             <div>|</div>
             <Link to='' onClick={logout} className='hover:underline'>Sair</Link>
