4d5fa2c76c425e661eaf3705dd02944520f627f8
Taylan Santos <taylanmf@gmail.com>
2024-03-07 18:08:17 -0300

    NavBar

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -12,13 +12,17 @@ function Footer() {
   if(usuario.token !== '') {
     footerComponent = (
       <>
-        <div className="flex justify-center bg-[#EA9050] text-white">
-          <div className="container flex flex-col items-center py-4">
-          <img src="./src/assets/img/png/logo-novo.png" alt="logo projeto cabe ação" className='w-8 h-8' />
+        <div className="flex justify-between bg-[#EA9050] text-white">
+          <div className="container flex justify-around items-center py-4">
+         
+         <div>  
+          {/* <img src="./src/assets/img/png/logo-novo.png" alt="logo projeto cabe ação"
+            className='w-14 h-14' /> */}
             <p className='text-xl font-bold'>Cabe Ação | Copyright: </p>
+          </div>
             <p className='text-lg'>Acesse nossas redes sociais</p>
             <div className='flex gap-2'>
-              <LinkedinLogo size={48} weight='bold' />
+              <LinkedinLogo size={48} weight='bold'  /> 
               <InstagramLogo size={48} weight='bold' />
               <FacebookLogo size={48} weight='bold' />
             </div>
